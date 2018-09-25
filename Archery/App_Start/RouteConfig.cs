8f36468ec1dadf6eeee93f5463e0d2d2d18965a2
sertoi <nguyentuongvien@yahoo.com>
2018-09-25 09:16:40 +0200

    Commit Vien

--- Archery/App_Start/RouteConfig.cs
+++ Archery/App_Start/RouteConfig.cs
@@ -12,12 +12,19 @@ namespace Archery
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.MapMvcAttributeRoutes();    //ajouter cette ligne pour ajouter un chemin a-propos
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+			routes.MapRoute(                               //Pour marcher le chemin comme localhoste:(58482/a-propos
+				name: "AboutRoute",
+				url: "a-propos",
+				defaults: new {Controller= "Home",action ="About"}
+		    );
+
         }
     }
 }
---
+++ Archery/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Archery.Models;
+
+namespace Archery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+		[Route ("a-propos")]			//Pour marcher le chemin comme localhoste:(58482/a-propos
+		public ActionResult About()
+		{
+			var info = new Info()
+			{
+			DevName = "Gtm",
+			ContactMail="Gtm@gmail.com",
+			CreatedDate= DateTime.Now			
+			};			
+			return View(info);
+		}
+    }
+
+}
