900f31f478223c9b3bc619705807102d9fbb8bf4
sertoi <nguyentuongvien@yahoo.com>
2018-09-26 12:20:17 +0200

    add model

---
+++ Archery/Controllers/ArchersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Controllers
+{
+    public class ArchersController : Controller
+    {
+        // GET: Players
+        public ActionResult Subscribe()
+        {
+            return View();
+        }
+		[HttpPost]		//Recouperer l'information de Html.
+		public ActionResult Subscribe(string email)
+		{
+			return View(); 
+		}
+    }
+}
---
+++ Archery/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Archery.Models
+{
+	public abstract class User
+	{
+		public int Id { get; set; }
+
+		public string Mail { get; set; }
+
+		[DataType(DataType.Password)]       //pour cacher Password sur l'ecran
+		public string Password { get; set; }
+
+		public string ConfirmedPassword { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		[DataType(DataType.Date)]		//au niveau Html
+		public DateTime BirthDate { get; set; }
+    
+	}
+}
